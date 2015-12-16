f0ec175e8a0998fab3af87f6e352d5d9773d0b3c
dajposkakac <dajposkakac@gmail.com>
2015-12-16 00:57:45 +0000

    app sends its request in xml instead of plain string

--- DirectionServer/src/transportapp/co600/directionserver/RequestHandler.java
+++ DirectionServer/src/transportapp/co600/directionserver/RequestHandler.java
@@ -3,7 +3,16 @@ package transportapp.co600.directionserver;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.net.Socket;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 public class RequestHandler extends Thread {
 	private Socket socket;
@@ -17,12 +26,13 @@ public class RequestHandler extends Thread {
 	public void run()	{
 		try	{
 		    bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream())); //get the client message
-		    String data = bufferedReader.readLine();
-		    String[] newData = data.split("!.!");
-//		    DirectionsRequest request = new DirectionsRequest(newData[0], newData[1], newData[2]);
-//		    DirectionsResult result = new DirectionsResult(request.getRoutes());
-//		    String resultString = result.getOrigin() + " -> " + result.getDestination() + ", " + result.getDistance() + ", " + result.getDuration() + "\n";
-		    String resultString = "test";
+		    Document xmlDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(bufferedReader.readLine())));
+		    HashMap<String, String> data = parseToMap(xmlDoc);
+		    DirectionsRequest request = new DirectionsRequest(data.get("origin"), data.get("destination"), data.get("transitMode"));
+		    DirectionsResult result = new DirectionsResult(request.getRoutes());
+		    
+		    String resultString = result.getOrigin() + " -> " + result.getDestination() + ", " + result.getDistance() + ", " + result.getDuration() + "\n";
+//		    String resultString = "test";
 		    printWriter = new PrintWriter(socket.getOutputStream(), true);
 		    System.out.println(resultString);
 		    printWriter.write(resultString);
@@ -38,4 +48,36 @@ public class RequestHandler extends Thread {
 			}	catch(Exception e)	{}
 		}
 	}
+	
+	private HashMap<String,String> parseToMap(Document doc)	{
+		NodeList nodes = doc.getFirstChild().getChildNodes();
+		HashMap<String, String> map = new HashMap<>();
+		for(int i = 0; i < nodes.getLength(); i++)  {
+            Node n = nodes.item(i);
+            String nodeName = n.getNodeName();
+        	map.put(nodeName, n.getTextContent());
+        }
+		return map;
+	}
+	
+//	private String createXMLResult(DirectionsResult res) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+//        Document xmlDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(activity.getResources().openRawResource(R.raw.request_template));
+//        NodeList nodes = xmlDoc.getFirstChild().getChildNodes();
+//        for(int i = 0; i < nodes.getLength(); i++)  {
+//            Node n = nodes.item(i);
+//            String nn = n.getNodeName();
+//            if(nn.equals("origin"))    {
+//                n.setTextContent(req.getOrigin());
+//            }   else if(nn.equals("destination"))   {
+//                n.setTextContent(req.getDestination());
+//            }   else if(nn.equals("transitMode"))  {
+//                n.setTextContent(req.getTransitMode());
+//            }
+//        }
+//        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+//        StreamResult sr = new StreamResult(new StringWriter());
+//        DOMSource source = new DOMSource(xmlDoc);
+//        transformer.transform(source, sr);
+//        return sr.getWriter().toString() + "\n";
+//    }
 }
--- GoogleDirectionsTest/app/src/main/java/transportapp/co600/googledirectionstest/ReceiveDirectionsTask.java
+++ GoogleDirectionsTest/app/src/main/java/transportapp/co600/googledirectionstest/ReceiveDirectionsTask.java
@@ -2,8 +2,11 @@ package transportapp.co600.googledirectionstest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.XmlResourceParser;
 import android.os.AsyncTask;
 import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -29,6 +32,25 @@ public class ReceiveDirectionsTask extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... params) {
         try {
+//            XmlResourceParser xmlFile = activity.getResources().getXml(R.xml.RequestTemplate);
+//            if (xmlFile.getEventType() == XmlPullParser.START_TAG)  {
+//                String name = xmlFile.getName();
+//                if(name.equals("request"))  {
+//                    xmlFile.next();
+//                    if(xmlFile.getName() != null && xmlFile.getName().equals("origin")) {
+//                        req.setOrigin(xmlFile.getText());
+//                        xmlFile.next();
+//                    }
+//                    if(xmlFile.getName() != null && xmlFile.getName().equals("destination"))    {
+//                        req.setOrigin(xmlFile.getText());
+//                        xmlFile.next();
+//                    }
+//                    if(xmlFile.getName() != null && xmlFile.getName().equals("transitMode"))    {
+//                        req.setTransitMode(xmlFile.getText());
+//                        xmlFile.next();
+//                    }
+//                }
+//            }
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream())); //get the client message
             Log.d("result", bufferedReader.readLine());
             //Log.d(TAG, s);
