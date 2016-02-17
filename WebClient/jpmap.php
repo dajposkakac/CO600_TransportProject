efedcfb29cd40d3efc23f7f514995b5d581be9c5
filipek123 <fb225@kent.ac.uk>
2016-02-17 20:14:46 +0000

    Website is now able to print out the result
    The result is being printed as a text although it can be easily changed into the list for JS purposes which is the next step. Peace

--- WebClient/jpmap.php
+++ WebClient/jpmap.php
@@ -1,25 +1,27 @@
 <?php
-	header("Content-Type: text/javascript; charset=utf-8");
-	$host = '192.168.1.2';
-	$port = 4444;
-	//$fp = fsockopen($host, $port, $errno, $errstr, 30);
+		header("Content-Type: text/javascript; charset=utf-8");
+		$host = '86.183.210.224';
+		$port = 4444;
 	// create socket
-	$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+		$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 	// connect to server
-	if(($result = socket_connect($socket, $host, $port)) === FALSE)
-		echo "socket_connect() error.\n" . socket_strerror(socket_last_error($socket));
-	else	{
+		$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n"); 		
 		$start = $_POST['start'];
 		$end = $_POST['end'];
 		$mode = $_POST['mode'];
 		$message = "<?xml version = '1.0' encoding = 'UTF-8'?><request><origin>" . $start . "</origin><destination>" . $end . "</destination><transitMode>" . $mode . "</transitMode></request>" . PHP_EOL;
-		// send string to server
+	// send string to server
+		if($result) { 
 		socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
-		// get server response
+	// get server response
 		$result = socket_read ($socket, 1024) or die("Could not read server response\n");
-		echo "Reply From Server  :".$result ."<br>";
-	}
-	
-	// close socket
-	//socket_close($socket);
+		}
+		$xml = simplexml_load_string($result);
+		$destination = $xml->info->destination;
+		$origin = $xml->info->origin;
+		echo "You can travel from " . $origin . " to" . PHP_EOL. "\n";
+		echo $destination . " by:". PHP_EOL ."\n";
+		foreach($xml->results->result as $rs) {
+		echo $rs->transitMode ." ". $rs->distance ." over ". $rs->duration ." at the price of £". $rs->price . PHP_EOL ."\n";
+		}
 ?>
