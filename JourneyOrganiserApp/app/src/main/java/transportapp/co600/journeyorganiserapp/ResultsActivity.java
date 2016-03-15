aed6cacaabb56da21bffc46260e2cf394c4f009d
MalyPM <mfm9@kent.ac.uk>
2016-03-15 20:38:35 +0000

    added the list switcher

--- JourneyOrganiserApp/app/src/main/java/transportapp/co600/journeyorganiserapp/ResultsActivity.java
+++ JourneyOrganiserApp/app/src/main/java/transportapp/co600/journeyorganiserapp/ResultsActivity.java
@@ -6,28 +6,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
+import org.honorato.multistatetogglebutton.ToggleButton;
 
 import java.util.HashMap;
 
 public class ResultsActivity extends AppCompatActivity {
 
+    private static final String TAG = "resultsActivity";
     private HashMap<String, String> info;
     private HashMap<Integer, HashMap<String, String>> results;
+
+    private ViewFlipper viewFlipper;
+    private float lastX;
+    private MultiStateToggleButton listSwitcher;
+
 
     @SuppressWarnings("unchecked")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        viewFlipper = (ViewFlipper) findViewById(R.id.view_flipper);
+
+        listSwitcher = (MultiStateToggleButton) findViewById(R.id.mstb_multi_id);
+
+        listSwitcher.setOnValueChangedListener(new ToggleButton.OnValueChangedListener() {
+            @Override
+            public void onValueChanged(int position) {
+                Log.d(TAG, "Button Selected : " + position);
+                viewFlipper.setDisplayedChild(position);
+            }
+        });
+
         info = (HashMap<String, String>) getIntent().getSerializableExtra("info");
         results = (HashMap<Integer, HashMap<String, String>>) getIntent().getSerializableExtra("results");
-        ListView resultsList = (ListView) findViewById(R.id.list);
+        ListView distanceList = (ListView) findViewById(R.id.list_distance);
         ResultsAdapter resultsAdapter = new ResultsAdapter(this, info, results);
-        resultsList.setAdapter(resultsAdapter);
+        distanceList.setAdapter(resultsAdapter);
         Log.d("adapter", "" + resultsAdapter.getCount());
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+
+        ListView timeList = (ListView) findViewById(R.id.list_time);
+        ResultsAdapter timeListAdapter = new ResultsAdapter(this, info, results);
+        timeList.setAdapter(timeListAdapter);
+
+        ListView costList = (ListView) findViewById(R.id.list_cost);
+        ResultsAdapter costListAdapter = new ResultsAdapter(this, info, results);
+        costList.setAdapter(costListAdapter);
+
+        viewFlipper.setDisplayedChild(0);
+        listSwitcher.setValue(0);
     }
 
     @Override
@@ -46,5 +77,4 @@ public class ResultsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
