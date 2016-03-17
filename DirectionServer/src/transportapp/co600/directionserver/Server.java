3b4b52301b04a5ef6769556c44331697f090fe10
dajposkakac <dajposkakac@gmail.com>
2016-03-17 19:21:36 +0000

    fixed issue where the server would try to start when it was unable to listen on port

--- DirectionServer/src/transportapp/co600/directionserver/Server.java
+++ DirectionServer/src/transportapp/co600/directionserver/Server.java
@@ -11,24 +11,23 @@ public class Server {
     	try {
     	    serverSocket = new ServerSocket(4444); //Server socket
     	    System.out.println("Host IP: " + InetAddress.getLocalHost().getHostAddress());
+    	    System.out.println("Server started. Listening to the port 4444");
+    	    try	{
+    			while(true)	{
+    				new RequestHandler(serverSocket.accept()).start();//accept the client connection
+    			}
+    		} catch(IOException e)	{
+    			System.out.println("Problem in message reading");
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}	finally	{
+    			try {
+    				serverSocket.close();
+    			} catch (IOException e) {}
+    		}
 	    } catch (IOException e) {
     	    System.out.println("Could not listen on port: 4444");
 	    }
-    	System.out.println("Server started. Listening to the port 4444");
-    	
-		try	{
-			while(true)	{
-				new RequestHandler(serverSocket.accept()).start();//accept the client connection
-			}
-		} catch(IOException e)	{
-			System.out.println("Problem in message reading");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}	finally	{
-			try {
-				serverSocket.close();
-			} catch (IOException e) {}
-		}
     }
     
     
