2b93bd773c922f4bc39fb3478127465dc2549522
dajposkakac <dajposkakac@gmail.com>
2016-03-18 02:41:41 +0000

    added map UI elements

--- JourneyOrganiserApp/app/src/main/java/transportapp/co600/journeyorganiserapp/DetailedResultActivity.java
+++ JourneyOrganiserApp/app/src/main/java/transportapp/co600/journeyorganiserapp/DetailedResultActivity.java
@@ -5,18 +5,27 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.HashMap;
 
 /**
  * Created by daj on 15/03/2016.
  */
-public class DetailedResultActivity extends AppCompatActivity {
+public class DetailedResultActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public static final String INFO_TAG = "info";
     public static final String RESULT_TAG = "result";
 
     private HashMap<String, String> info;
     private HashMap<String, String> results;
+
+    private GoogleMap mMap;
 
     @SuppressWarnings("unchecked")
     @Override
@@ -56,5 +65,17 @@ public class DetailedResultActivity extends AppCompatActivity {
             arriveAt.setText(arrivalTimeDate);
         }
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng canters = new LatLng(51.2750, 1.0870);
+        mMap.addMarker(new MarkerOptions().position(canters));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(canters));
     }
 }
